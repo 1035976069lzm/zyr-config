9bc37712a451ba20a5d6c809fcaae52a9ae3cf96
liuzunming <1035976069@qq.com>
2019-07-30 16:46:27 +0800

    first commit

---
+++ src/main/java/zyr/learn/config/zyrconfig/ZyrConfigApplication.java
@@ -0,0 +1,13 @@
+package zyr.learn.config.zyrconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyrConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyrConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zyr/learn/config/zyrconfig/ZyrConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package zyr.learn.config.zyrconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZyrConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
